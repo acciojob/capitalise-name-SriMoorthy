7db73a3ed5bf49c159c0017ffdc63673ec50e913
SriMoorthy <srimoorthy27@gmail.com>
2024-07-13 16:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let Fname = document.getElementById('fname')
+let val = Fname.innerHTML.toUpperCase()
+
+Fname.addEventListener('mouseleave', () => {
+	document.getElementById('fname').value = val
+})
